c1c9134ff3c7177f02d8b422f9106cbd0f34ab8b
abby-pfingsten <abbypfin@yahoo.com>
2024-01-16 08:20:59 -0500

    printing elements from array

--- front/js/script.js
+++ front/js/script.js
@@ -5,10 +5,21 @@ fetch("http://localhost:3000/api/products")
   .then((data) => {
     return data.json();
   })
-  .then((sections) => {
+    .then((sections) => {
+        insertProdOnPage(sections);
     // console.log(sections);
   });
 
 // get existing element from page
 const sectionHolder = document.querySelector('section');
-console.log(sectionHolder);
+// console.log(sectionHolder);
+
+// create function to insert product into HTML
+function insertProdOnPage(sections) {
+    for (let i in sections) {
+        const section = sections[i];
+        console.log(section);
+
+    }
+
+}
