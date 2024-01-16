ae80c5a9a44aac16941cabb1d238e416598d0ee5
abby-pfingsten <abbypfin@yahoo.com>
2024-01-16 09:48:54 -0500

    m3 complete

--- front/js/script.js
+++ front/js/script.js
@@ -5,36 +5,33 @@ fetch("http://localhost:3000/api/products")
   .then((data) => {
     return data.json();
   })
-    .then((sections) => {
-        insertProdOnPage(sections);
+  .then((sections) => {
+    insertProdOnPage(sections);
     // console.log(sections);
   });
 
 // get existing element from page
-const sectionHolder = document.querySelector('section');
+const sectionHolder = document.querySelector("section");
 // console.log(sectionHolder);
 
 // create function to insert product into HTML
 function insertProdOnPage(sections) {
-    for (let i in sections) {
-        const section = sections[i];
-        console.log(section);
+  for (let i in sections) {
+    const section = sections[i];
 
-        const newSection = document.createElement('section');
+    // we are creating new a section
+    const newSection = document.createElement("a");
 
-        newSection.classList.add('items');
-        newSection.innerHTML = `
-        <a href="./product.html?id=${section._id}"
+    // set the attribute so that the href is unique to each prod
+    newSection.setAttribute("href", `./product.html?id=${section._id}`);
+    newSection.innerHTML = `
             <article>
-                <img src=${section.imgUrl} alt=${section.altTxt}>
-                <h3 class=productName>${section.name}</h3>
-                <p class= productDescription>${section.description}</p>
+                <img src=${section.imageUrl} alt=${section.altTxt}>
+                <h3 class="productName">${section.name}</h3>
+                <p class="productDescription">${section.description}</p>
             </article>
-        </a>
         `;
 
-        sectionHolder.appendChild(newSection);
-
-    }
-
+    sectionHolder.appendChild(newSection);
+  }
 }
