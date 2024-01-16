b37f79356face17b698f329e42a51c3ef2623338
abby-pfingsten <abbypfin@yahoo.com>
2024-01-16 08:11:02 -0500

    beginning m3

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,14 @@
-console.log('helloworld');
+console.log("helloworld");
+
+// grab the data from the backend
+fetch("http://localhost:3000/api/products")
+  .then((data) => {
+    return data.json();
+  })
+  .then((sections) => {
+    // console.log(sections);
+  });
+
+// get existing element from page
+const sectionHolder = document.querySelector('section');
+console.log(sectionHolder);
