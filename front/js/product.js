be9516d05b2a634383b302629eafc21be1f512be
abby-pfingsten <abbypfin@yahoo.com>
2024-01-17 17:10:53 -0500

    getting error but wrote function to insert stuff into dom

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,66 @@
+/* Grab Elements from the Page */
+const productImage = document.getElementsByClassName("item__img");
+// console.log(productImage);
+
+const productTitle = document.getElementById("title");
+// console.log(productTitle);
+
+const productDescription = document.getElementById("description");
+// console.log(productPrice);
+
+const productPrice = document.getElementById("price");
+// console.log(productPrice);
+
+const dropdown = document.getElementsByTagName("select");
+// console.log(dropdown);
+
+/* Insert Into Dom Func */
+
+// create an array with all of the elements that
+// need to be edited with API data
+productPageDOMChanges = [productImage, productTitle, productPrice, dropdown];
+
+// create function to insert product into HTML
+function insertProdOnPage(
+  response,
+  productImage,
+  productTitle,
+  productDescription,
+  productPrice,
+  dropdown
+) {
+  //   for (let i in sections) {
+  // productImage
+  const imgSection = document.createElement("img");
+  imgSection.setAttribute("src", response.imageURl);
+  imgSection.setAttribute("src", response.altTxt);
+  productImage.appendChild(imgSection);
+
+  // productTitle
+  productTitle.textContent = response.name;
+
+  // productDescription
+  productDescription.textContent = response.description;
+
+  //   // we are creating new a section
+  //   const newSection = document.createElement("a");
+
+  //   // set the attribute so that the href is unique to each prod
+  //   newSection.setAttribute("href", `./product.html?id=${section._id}`);
+  //   newSection.innerHTML = `
+  //             <article>
+  //                 <img src=${section.imageUrl} alt=${section.altTxt}>
+  //                 <h3 class="productName">${section.name}</h3>
+  //                 <p class="productDescription">${section.description}</p>
+  //             </article>
+  //         `;
+
+  //   sectionHolder.appendChild(newSection);
+  //   }
+}
+
+/* URL Content and API Call*/
+
 // grab the current url
 const url = new URL(document.URL);
 
@@ -14,5 +77,16 @@ apiRequest.send();
 // onreadystatechange function
 apiRequest.onreadystatechange = () => {
   if (apiRequest.readyState === 4) {
+    const response = JSON.parse(apiRequest.response);
+    console.log(response);
+    insertProdOnPage(
+      response,
+      productImage,
+      productTitle,
+      productDescription,
+      productPrice,
+      dropdown
+    );
+    //   call insert dom function
   }
 };
